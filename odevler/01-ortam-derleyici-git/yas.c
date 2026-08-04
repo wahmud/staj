d4187754a8bf6f03918a6cc83daa4af721cd06c6
wahmud <hacimahmud9@gmail.com>
2026-08-04 13:28:25 +0300

    yas programı tamamlandı

---
+++ odevler/01-ortam-derleyici-git/yas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define   MAX_NAME_LEN   40
+#define   CURRENT_YEAR   2026
+int main(void)
+{
+    printf("lutfen adinizi girin: ");
+    char name[MAX_NAME_LEN];
+    scanf("%s", name);
+
+    printf("dogum yilinizi girin: ");
+    int date;
+    scanf("%d", date);
+
+    printf("Merhaba %s! bu yil %d yasindasin\n", name, CURRENT_YEAR - date);
+
+}
