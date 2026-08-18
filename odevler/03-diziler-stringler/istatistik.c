b1cfecdcc0f174b28e186f4c4a645818408a5b15
wahmud <hacimahmud9@gmail.com>
2026-08-18 15:49:51 +0300

    istatistik.c'de kelime sayisi hesabi eklendi

--- odevler/03-diziler-stringler/istatistik.c
+++ odevler/03-diziler-stringler/istatistik.c
@@ -22,35 +22,32 @@ int main(void)
     //printf("%s\n", metin);
 
  
-    // int alpha_cnt = 0;
-    // int digit_cnt = 0;
-    // int space_cnt = 0;
-    // int total_cnt = 0;
-    // for (int i = 0; metin[i] != '\0'; ++i) {
-    //     alpha_cnt += !!isalpha(metin[i]);
-    //     digit_cnt += !!isdigit(metin[i]);
-    //     space_cnt += !!isspace(metin[i]);
-    //     ++total_cnt;
-    // }
-    // printf("harf sayisi   = %d\n", alpha_cnt);
-    // printf("rakam sayisi  = %d\n", digit_cnt);
-    // printf("bosluk sayisi = %d\n", space_cnt);
-    // printf("toplam karakter sayisi   = %d\n", total_cnt);
     int alpha_cnt = 0;
     int digit_cnt = 0;
     int space_cnt = 0;
     int total_cnt = 0;
-    for (int i = 0; i < strlen(metin); ++i) {
+    for (int i = 0; metin[i] != '\0'; ++i) {
         alpha_cnt += !!isalpha(metin[i]);
         digit_cnt += !!isdigit(metin[i]);
         space_cnt += !!isspace(metin[i]);
         ++total_cnt;
     }
-    printf("harf sayisi   = %d\n", alpha_cnt);
-    printf("rakam sayisi  = %d\n", digit_cnt);
-    printf("bosluk sayisi = %d\n", space_cnt);
-    printf("toplam karakter sayisi   = %d\n", total_cnt);
-
-
+    // printf("harf sayisi   = %d\n", alpha_cnt);
+    // printf("rakam sayisi  = %d\n", digit_cnt);
+    // printf("bosluk sayisi = %d\n", space_cnt);
+    // printf("toplam karakter sayisi   = %d\n", total_cnt);
+    
+    int word_cnt = 0;
+    for (int i = 1; i < strlen(metin); ++i) {
+        int is_space = 0;
+        is_space = metin[i - 1] == ' ';
+        if (is_space && (isalpha(metin[i]) || isdigit(metin[i]))) {
+            ++word_cnt;
+        }
+        else if (i == 1 && (isalpha(metin[i - 1]) || isdigit(metin[i - 1]))) {
+            ++word_cnt;
+        }
+    }
+    printf("kelime sayisi = %d\n", word_cnt);
 
 }
